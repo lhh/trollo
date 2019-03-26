8f3ee5a1f02410f805159cb9ad2b53a76eb637bc
Jason Joyce <jjoyce@redhat.com>
2019-03-26 08:21:04 -0400

    Adding an api endpoint to change the position of a checklist item.
    Bumped to version 0.1.8
    
    Signed-off-by: Jason Joyce <fuzzball81@gmail.com>

--- trollo/cards.py
+++ trollo/cards.py
@@ -148,6 +148,14 @@ class Cards(object):
         resp.raise_for_status()
         return json.loads(resp.content)
 
+    def move_checkItem(self, card_id, idCheckItem, position):
+        """
+        Valid postions: 'top', 'bottom' or a positive number
+        """
+        resp = requests.put("https://trello.com/1/cards/%s/checkItem/%s" % (card_id, idCheckItem), params=dict(key=self._apikey, token=self._token), data=dict(pos=str(position)))
+        resp.raise_for_status()
+        return json.loads(resp.content)
+
     def new(self, name, idList, desc=None):
         resp = requests.post("https://trello.com/1/cards" % (), params=dict(key=self._apikey, token=self._token), data=dict(name=name, idList=idList, desc=desc))
         resp.raise_for_status()
