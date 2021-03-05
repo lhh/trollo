f9811dfb1e97b351dd105a29002dc7f7ad82f125
Lon Hohberger <lon@metamorphism.com>
2021-03-04 19:12:27 -0500

    Allow updating labels
    Signed-off-by: Lon Hohberger <lon@metamorphism.com>

--- trollo/labels.py
+++ trollo/labels.py
@@ -13,6 +13,22 @@ class Labels(object):
         self._apikey = apikey
         self._token = token
 
+    def get(self, label_id):
+        resp = requests.get("https://trello.com/1/labels/%s" % (label_id), params=dict(key=self._apikey, token=self._token), data=None)
+        resp.raise_for_status()
+        return json.loads(resp.content)
+
+    def update(self, label_id, name=None, color=None):
+        if (name and color) or (not name and not color):
+            resp = requests.put("https://trello.com/1/labels/%s" % (label_id), params=dict(key=self._apikey, token=self._token, name=name, color=color), data=None)
+        elif name:
+            resp = requests.put("https://trello.com/1/labels/%s/name" % (label_id), params=dict(key=self._apikey, token=self._token, value=name), data=None)
+        elif color:
+            resp = requests.put("https://trello.com/1/labels/%s/color" % (label_id), params=dict(key=self._apikey, token=self._token, value=color), data=None)
+
+        resp.raise_for_status()
+        return json.loads(resp.content)
+
     def delete(self, label_id):
         resp = requests.delete("https://trello.com/1/labels/%s" % (label_id), params=dict(key=self._apikey, token=self._token), data=None)
         resp.raise_for_status()
